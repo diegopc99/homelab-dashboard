5568d020341ea06d6de2eb2851e1b1bb67ec9ca5
Diego <diegopc99@gmail.com>
2025-11-18 16:15:25 +0100

    feat: added new services and reimplemnted dashboard style

--- src/data/services.js
+++ src/data/services.js
@@ -1,16 +1,8 @@
-import grafanaLogo from '/public/grafana-logo.svg';
-import prometheusLogo from '/public//prometheus-logo.svg';
-import wireguardLogo from '/public//wireguard-logo.svg';
-import piholeLogo from '/public//pihole-logo.svg';
-import argocdLogo from '/public//argocd-logo.svg';
-import traefikLogo from '/public//traefik-logo.svg';
-import harborLogo from '/public//harbor-logo.svg';
-
-export const services = [
+export const infrastructureServices = [
   {
     name: "Grafana",
     href: "https://monitoring.homeserver.local/grafana",
-    logo: grafanaLogo,
+    logo: "/grafana-logo.svg",
     description: "Metrics & Dashboards",
     tag: "Analytics",
     tagClass: "text-violet-400 bg-violet-500/20",
@@ -18,7 +10,7 @@ export const services = [
   {
     name: "Prometheus",
     href: "https://monitoring.homeserver.local/prometheus",
-    logo: prometheusLogo,
+    logo: "/prometheus-logo.svg",
     description: "Monitoring & Alerts",
     tag: "Monitoring",
     tagClass: "text-emerald-400 bg-emerald-500/20",
@@ -26,7 +18,7 @@ export const services = [
   {
     name: "VPN Dashboard",
     href: "https://vpn.homeserver.local",
-    logo: wireguardLogo,
+    logo: "/wireguard-logo.svg",
     description: "Remote Access",
     tag: "Security",
     tagClass: "text-blue-400 bg-blue-500/20",
@@ -34,7 +26,7 @@ export const services = [
   {
     name: "Pi-hole",
     href: "http://pihole.homeserver.local/admin/login",
-    logo: piholeLogo,
+    logo: "/pihole-logo.svg",
     description: "Network Ad Blocking",
     tag: "DNS Filter",
     tagClass: "text-red-400 bg-red-500/20",
@@ -42,7 +34,7 @@ export const services = [
   {
     name: "ArgoCD",
     href: "https://argocd.homeserver.local",
-    logo: argocdLogo,
+    logo: "/argocd-logo.svg",
     description: "GitOps Continuous Delivery",
     tag: "GitOps",
     tagClass: "text-cyan-400 bg-cyan-500/20",
@@ -50,7 +42,7 @@ export const services = [
   {
     name: "Traefik",
     href: "https://traefik.homeserver.local/dashboard/",
-    logo: traefikLogo,
+    logo: "/traefik-logo.svg",
     description: "Ingress Controller Dashboard",
     tag: "Ingress",
     tagClass: "text-orange-400 bg-orange-500/20",
@@ -58,9 +50,30 @@ export const services = [
   {
     name: "Harbor Registry",
     href: "https://registry.homeserver.local",
-    logo: harborLogo,
+    logo: "/harbor-logo.svg",
     description: "Container Image Registry",
     tag: "Registry",
     tagClass: "text-teal-400 bg-teal-500/20",
   },
+  {
+    name: "MinIO Console",
+    href: "https://minio.homeserver.local",
+    logo: "/minio-logo.svg",
+    description: "Object Storage Management",
+    tag: "Storage",
+    tagClass: "text-rose-400 bg-rose-500/20",
+  },
 ];
+
+export const contentServices = [
+  {
+    name: "Nextcloud",
+    href: "https://nextcloud.homeserver.local",
+    logo: "/nextcloud-logo.svg",
+    description: "File Sharing & Collaboration",
+    tag: "Productivity",
+    tagClass: "text-sky-400 bg-sky-500/20",
+  },
+]
+
+export const services = [...infrastructureServices, ...contentServices];
